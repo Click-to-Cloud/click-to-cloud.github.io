3ee16065de5ba93a6799194a131f9317316b0af6
Miao (Wilson) Liu <wilson@clicktocloud.com>
2018-05-11 09:59:36 +1000

    add docs

---
+++ js/bundles/pageIndex.js
@@ -0,0 +1,436 @@
+var pageComponent =
+webpackJsonppageComponent([0],{
+
+/***/ 172:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _metalComponent = __webpack_require__(2);
+
+var _metalComponent2 = _interopRequireDefault(_metalComponent);
+
+var _metalSoy = __webpack_require__(3);
+
+var _metalSoy2 = _interopRequireDefault(_metalSoy);
+
+__webpack_require__(31);
+
+__webpack_require__(38);
+
+__webpack_require__(39);
+
+__webpack_require__(40);
+
+__webpack_require__(41);
+
+__webpack_require__(42);
+
+__webpack_require__(43);
+
+__webpack_require__(44);
+
+__webpack_require__(45);
+
+__webpack_require__(46);
+
+__webpack_require__(47);
+
+__webpack_require__(48);
+
+var _indexSoy = __webpack_require__(261);
+
+var _indexSoy2 = _interopRequireDefault(_indexSoy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var pageIndex = function (_Component) {
+  _inherits(pageIndex, _Component);
+
+  function pageIndex() {
+    _classCallCheck(this, pageIndex);
+
+    return _possibleConstructorReturn(this, (pageIndex.__proto__ || Object.getPrototypeOf(pageIndex)).apply(this, arguments));
+  }
+
+  return pageIndex;
+}(_metalComponent2.default);
+
+;
+
+_metalSoy2.default.register(pageIndex, _indexSoy2.default);
+
+exports.default = pageIndex;
+
+/***/ }),
+
+/***/ 261:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.templates = exports.pageIndex = undefined;
+
+var _metalComponent = __webpack_require__(2);
+
+var _metalComponent2 = _interopRequireDefault(_metalComponent);
+
+var _metalSoy = __webpack_require__(3);
+
+var _metalSoy2 = _interopRequireDefault(_metalSoy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /* jshint ignore:start */
+
+
+var templates;
+goog.loadModule(function (exports) {
+
+  // This file was automatically generated from index.soy.
+  // Please don't edit this file by hand.
+
+  /**
+   * @fileoverview Templates in namespace pageIndex.
+   * @public
+   */
+
+  goog.module('pageIndex.incrementaldom');
+
+  /** @suppress {extraRequire} */
+  var soy = goog.require('soy');
+  /** @suppress {extraRequire} */
+  var soydata = goog.require('soydata');
+  /** @suppress {extraRequire} */
+  goog.require('goog.asserts');
+  /** @suppress {extraRequire} */
+  goog.require('soy.asserts');
+  /** @suppress {extraRequire} */
+  goog.require('goog.i18n.bidi');
+  /** @suppress {extraRequire} */
+  goog.require('goog.string');
+  var IncrementalDom = goog.require('incrementaldom');
+  var ie_open = IncrementalDom.elementOpen;
+  var ie_close = IncrementalDom.elementClose;
+  var ie_void = IncrementalDom.elementVoid;
+  var ie_open_start = IncrementalDom.elementOpenStart;
+  var ie_open_end = IncrementalDom.elementOpenEnd;
+  var itext = IncrementalDom.text;
+  var iattr = IncrementalDom.attr;
+
+  var $templateAlias2 = _metalSoy2.default.getTemplate('Footer.incrementaldom', 'render');
+
+  var $templateAlias1 = _metalSoy2.default.getTemplate('main.incrementaldom', 'render');
+
+  /**
+   * @param {{
+   *    site: (?),
+   *    year: (?),
+   *    elementClasses: (?)
+   * }} opt_data
+   * @param {(null|undefined)=} opt_ignored
+   * @param {Object<string, *>=} opt_ijData
+   * @return {void}
+   * @suppress {checkTypes}
+   */
+  function $render(opt_data, opt_ignored, opt_ijData) {
+    var param234 = function param234() {
+      $header(opt_data, null, opt_ijData);
+      $why(null, null, opt_ijData);
+      $features(null, null, opt_ijData);
+      $how(null, null, opt_ijData);
+      $highlights(null, null, opt_ijData);
+      $templateAlias2(opt_data, null, opt_ijData);
+    };
+    $templateAlias1(soy.$$assignDefaults({ content: param234 }, opt_data), null, opt_ijData);
+  }
+  exports.render = $render;
+  if (goog.DEBUG) {
+    $render.soyTemplateName = 'pageIndex.render';
+  }
+
+  /**
+   * @param {{
+   *    site: (?)
+   * }} opt_data
+   * @param {(null|undefined)=} opt_ignored
+   * @param {Object<string, *>=} opt_ijData
+   * @return {void}
+   * @suppress {checkTypes}
+   */
+  function $header(opt_data, opt_ignored, opt_ijData) {
+    ie_open('header', null, null, 'class', 'header');
+    ie_open('div', null, null, 'class', 'container');
+    ie_open('h1', null, null, 'class', 'header-title');
+    var dyn22 = opt_data.site.title;
+    if (typeof dyn22 == 'function') dyn22();else if (dyn22 != null) itext(dyn22);
+    ie_close('h1');
+    ie_open('h2', null, null, 'class', 'header-subtitle');
+    var dyn23 = opt_data.site.index.description;
+    if (typeof dyn23 == 'function') dyn23();else if (dyn23 != null) itext(dyn23);
+    ie_close('h2');
+    ie_close('div');
+    ie_close('header');
+  }
+  exports.header = $header;
+  if (goog.DEBUG) {
+    $header.soyTemplateName = 'pageIndex.header';
+  }
+
+  /**
+   * @param {Object<string, *>=} opt_data
+   * @param {(null|undefined)=} opt_ignored
+   * @param {Object<string, *>=} opt_ijData
+   * @return {void}
+   * @suppress {checkTypes}
+   */
+  function $why(opt_data, opt_ignored, opt_ijData) {
+    ie_open('article', null, null, 'class', 'about');
+    ie_open('div', null, null, 'class', 'container');
+    ie_open('div', null, null, 'class', 'row');
+    ie_open('div', null, null, 'class', 'col-md-12 col-md-offset-2');
+    ie_open('h3', null, null, 'class', 'about-title');
+    itext('Apex Libraries');
+    ie_close('h3');
+    ie_open('p', null, null, 'class', 'about-description');
+    ie_open('a', null, null, 'href', 'http://www.clicktocloud.com/');
+    itext('Click-to-Cloud');
+    ie_close('a');
+    itext(' is a vendor and partner of Salesforce, and devotes itself in Salesforce development. During coding in Apex, we have met pain points and we are trying to resolve them. And these libraries are the tools that we have developed to ease the pain and make Salesforce Apex more friendly.');
+    ie_close('p');
+    ie_close('div');
+    ie_close('div');
+    ie_close('div');
+    ie_close('article');
+  }
+  exports.why = $why;
+  if (goog.DEBUG) {
+    $why.soyTemplateName = 'pageIndex.why';
+  }
+
+  /**
+   * @param {Object<string, *>=} opt_data
+   * @param {(null|undefined)=} opt_ignored
+   * @param {Object<string, *>=} opt_ijData
+   * @return {void}
+   * @suppress {checkTypes}
+   */
+  function $features(opt_data, opt_ignored, opt_ijData) {
+    ie_open('div', null, null, 'class', 'features');
+    ie_open('div', null, null, 'class', 'container');
+    ie_open('div', null, null, 'class', 'row');
+    ie_open('section', null, null, 'class', 'feature col-md-4 col-md-offset-2');
+    ie_open('h3', null, null, 'class', 'feature-title');
+    itext('Pluggable Libraries');
+    ie_close('h3');
+    ie_open('p', null, null, 'class', 'feature-description');
+    itext('Most of our Apex libraries are small and self-dependent, which makes it easy to include and use in your development.');
+    ie_close('p');
+    ie_close('section');
+    ie_open('section', null, null, 'class', 'feature col-md-4');
+    ie_open('h3', null, null, 'class', 'feature-title');
+    itext('Problem Oriented');
+    ie_close('h3');
+    ie_open('p', null, null, 'class', 'feature-description');
+    itext('Each of our libraries focuses on one specific problem and tries to solve it well.');
+    ie_close('p');
+    ie_close('section');
+    ie_open('section', null, null, 'class', 'feature col-md-4');
+    ie_open('h3', null, null, 'class', 'feature-title');
+    itext('Active maintanence');
+    ie_close('h3');
+    ie_open('p', null, null, 'class', 'feature-description');
+    itext('We are positive in using these libraries in our company and will keep maintaining them.');
+    ie_close('p');
+    ie_close('section');
+    ie_close('div');
+    ie_close('div');
+    ie_close('div');
+  }
+  exports.features = $features;
+  if (goog.DEBUG) {
+    $features.soyTemplateName = 'pageIndex.features';
+  }
+
+  /**
+   * @param {Object<string, *>=} opt_data
+   * @param {(null|undefined)=} opt_ignored
+   * @param {Object<string, *>=} opt_ijData
+   * @return {void}
+   * @suppress {checkTypes}
+   */
+  function $how(opt_data, opt_ignored, opt_ijData) {
+    ie_open('article', null, null, 'class', 'about');
+    ie_open('div', null, null, 'class', 'container');
+    ie_open('div', null, null, 'class', 'row');
+    ie_open('div', null, null, 'class', 'col-md-12 col-md-offset-2');
+    ie_open('h3', null, null, 'class', 'about-title');
+    itext('Easy to pick one up');
+    ie_close('h3');
+    ie_open('p', null, null, 'class', 'about-description');
+    itext('Our libraries are delibrately designed and easy-to-use. No extra configurations. Very few apex files. Detailed guides to help you get started.');
+    ie_close('p');
+    ie_close('div');
+    ie_close('div');
+    ie_close('div');
+    ie_close('article');
+  }
+  exports.how = $how;
+  if (goog.DEBUG) {
+    $how.soyTemplateName = 'pageIndex.how';
+  }
+
+  /**
+   * @param {Object<string, *>=} opt_data
+   * @param {(null|undefined)=} opt_ignored
+   * @param {Object<string, *>=} opt_ijData
+   * @return {void}
+   * @suppress {checkTypes}
+   */
+  function $highlights(opt_data, opt_ignored, opt_ijData) {
+    ie_open('div', null, null, 'class', 'highlights');
+    ie_open('div', null, null, 'class', 'container');
+    ie_open('section', null, null, 'class', 'highlight row');
+    ie_open('div', null, null, 'class', 'col-md-6 col-md-offset-2');
+    ie_open('h4', null, null, 'class', 'highlight-title');
+    ie_open('a', null, null, 'href', 'https://github.com/Click-to-Cloud/Query.apex');
+    itext('Query.apex');
+    ie_close('a');
+    ie_close('h4');
+    ie_open('p', null, null, 'class', 'highlight-description');
+    itext('Query.apex provides a flexible and dynamic way of building a SOQL query on the Salesforce platform.');
+    ie_close('p');
+    ie_close('div');
+    ie_open('div', null, null, 'class', 'col-md-5 col-md-offset-1');
+    ie_open('img', null, null, 'class', 'highlight-image', 'src', '/images/highlight_1.png', 'alt', 'Fluent API', 'height', '200');
+    ie_close('img');
+    ie_close('div');
+    ie_close('section');
+    ie_open('section', null, null, 'class', 'highlight row');
+    ie_open('div', null, null, 'class', 'col-md-6 col-md-push-9');
+    ie_open('h4', null, null, 'class', 'highlight-title');
+    ie_open('a', null, null, 'href', 'https://github.com/Click-to-Cloud/R.apex');
+    itext('R.apex');
+    ie_close('a');
+    ie_close('h4');
+    ie_open('p', null, null, 'class', 'highlight-description');
+    itext('R.apex is a functional utility library for Apex.');
+    ie_close('p');
+    ie_close('div');
+    ie_open('div', null, null, 'class', 'col-md-5 col-md-offset-2 col-md-pull-6');
+    ie_open('img', null, null, 'class', 'highlight-image', 'src', '/images/highlight_2.png', 'alt', 'Collection Support', 'height', '200');
+    ie_close('img');
+    ie_close('div');
+    ie_close('section');
+    ie_open('section', null, null, 'class', 'highlight row');
+    ie_open('div', null, null, 'class', 'col-md-6 col-md-offset-2');
+    ie_open('h4', null, null, 'class', 'highlight-title');
+    ie_open('a', null, null, 'href', 'https://github.com/Click-to-Cloud/Script.apex');
+    itext('Script.apex');
+    ie_close('a');
+    ie_close('h4');
+    ie_open('p', null, null, 'class', 'highlight-description');
+    itext('Script.apex evaluates Javascript expressions in Apex.');
+    ie_close('p');
+    ie_close('div');
+    ie_open('div', null, null, 'class', 'col-md-5 col-md-offset-1');
+    ie_open('img', null, null, 'class', 'highlight-image', 'src', '/images/highlight_3.png', 'alt', 'Function Composition', 'height', '200');
+    ie_close('img');
+    ie_close('div');
+    ie_close('section');
+    ie_open('section', null, null, 'class', 'highlight row');
+    ie_open('div', null, null, 'class', 'col-md-6 col-md-push-9');
+    ie_open('h4', null, null, 'class', 'highlight-title');
+    ie_open('a', null, null, 'href', 'https://github.com/Click-to-Cloud/Moment.apex');
+    itext('Moment.apex');
+    ie_close('a');
+    ie_close('h4');
+    ie_open('p', null, null, 'class', 'highlight-description');
+    itext('Moment.apex parses, manipulates and displays dates and times in Apex.');
+    ie_close('p');
+    ie_close('div');
+    ie_open('div', null, null, 'class', 'col-md-5 col-md-offset-2 col-md-pull-6');
+    ie_open('img', null, null, 'class', 'highlight-image', 'src', '/images/highlight_4.png', 'alt', 'Collection Support', 'height', '200');
+    ie_close('img');
+    ie_close('div');
+    ie_close('section');
+    ie_open('section', null, null, 'class', 'highlight row');
+    ie_open('div', null, null, 'class', 'col-md-6 col-md-offset-2');
+    ie_open('h4', null, null, 'class', 'highlight-title');
+    ie_open('a', null, null, 'href', 'https://github.com/Click-to-Cloud/Async.apex');
+    itext('Async.apex');
+    ie_close('a');
+    ie_close('h4');
+    ie_open('p', null, null, 'class', 'highlight-description');
+    itext('Async.apex is a promise-like functional library to handle asynchronous code.');
+    ie_close('p');
+    ie_close('div');
+    ie_open('div', null, null, 'class', 'col-md-5 col-md-offset-1');
+    ie_open('img', null, null, 'class', 'highlight-image', 'src', '/images/highlight_5.png', 'alt', 'Function Composition', 'height', '200');
+    ie_close('img');
+    ie_close('div');
+    ie_close('section');
+    ie_close('div');
+    ie_close('div');
+  }
+  exports.highlights = $highlights;
+  if (goog.DEBUG) {
+    $highlights.soyTemplateName = 'pageIndex.highlights';
+  }
+
+  exports.render.params = ["site"];
+  exports.render.types = { "site": "?" };
+  exports.header.params = ["site"];
+  exports.header.types = { "site": "?" };
+  exports.why.params = [];
+  exports.why.types = {};
+  exports.features.params = [];
+  exports.features.types = {};
+  exports.how.params = [];
+  exports.how.types = {};
+  exports.highlights.params = [];
+  exports.highlights.types = {};
+  exports.templates = templates = exports;
+  return exports;
+});
+
+var pageIndex = function (_Component) {
+  _inherits(pageIndex, _Component);
+
+  function pageIndex() {
+    _classCallCheck(this, pageIndex);
+
+    return _possibleConstructorReturn(this, (pageIndex.__proto__ || Object.getPrototypeOf(pageIndex)).apply(this, arguments));
+  }
+
+  return pageIndex;
+}(_metalComponent2.default);
+
+_metalSoy2.default.register(pageIndex, templates);
+exports.pageIndex = pageIndex;
+exports.templates = templates;
+exports.default = templates;
+/* jshint ignore:end */
+
+/***/ })
+
+},[172]);
