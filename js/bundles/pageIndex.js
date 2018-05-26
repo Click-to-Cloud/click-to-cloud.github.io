02876fe5c1d83f36c85b91318f19a56841796458
Miao (Wilson) Liu <wilson@clicktocloud.com>
2018-05-26 15:50:34 +1000

    update docs

--- js/bundles/pageIndex.js
+++ js/bundles/pageIndex.js
@@ -485,6 +485,22 @@ goog.loadModule(function (exports) {
     ie_close('img');
     ie_close('div');
     ie_close('section');
+    ie_open('section', null, null, 'class', 'highlight row');
+    ie_open('div', null, null, 'class', 'col-md-6 col-md-push-9');
+    ie_open('h4', null, null, 'class', 'highlight-title');
+    ie_open('a', null, null, 'href', 'https://github.com/Click-to-Cloud/Stream.apex');
+    itext('Stream.apex');
+    ie_close('a');
+    ie_close('h4');
+    ie_open('p', null, null, 'class', 'highlight-description');
+    itext('Stream.apex is a library that streamlines your Funcs in Apex.');
+    ie_close('p');
+    ie_close('div');
+    ie_open('div', null, null, 'class', 'col-md-5 col-md-offset-2 col-md-pull-6');
+    ie_open('img', null, null, 'class', 'highlight-image', 'src', '/images/highlight_12.png', 'alt', 'Stream.apex', 'height', '200');
+    ie_close('img');
+    ie_close('div');
+    ie_close('section');
     ie_close('div');
     ie_close('div');
   }
