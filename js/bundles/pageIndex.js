0f059cd52935136705555fe69d6f5e39781d09fd
Miao (Wilson) Liu <wilson@clicktocloud.com>
2018-06-18 13:18:43 +1000

    update docs

--- js/bundles/pageIndex.js
+++ js/bundles/pageIndex.js
@@ -549,6 +549,22 @@ goog.loadModule(function (exports) {
     ie_close('img');
     ie_close('div');
     ie_close('section');
+    ie_open('section', null, null, 'class', 'highlight row');
+    ie_open('div', null, null, 'class', 'col-md-6 col-md-push-9');
+    ie_open('h4', null, null, 'class', 'highlight-title');
+    ie_open('a', null, null, 'href', 'https://github.com/Click-to-Cloud/Pack.apex');
+    itext('Pack.apex');
+    ie_close('a');
+    ie_close('h4');
+    ie_open('p', null, null, 'class', 'highlight-description');
+    itext('Pack.apex is a tool to help you generate Func wrappers for existing Apex classes.');
+    ie_close('p');
+    ie_close('div');
+    ie_open('div', null, null, 'class', 'col-md-5 col-md-offset-2 col-md-pull-6');
+    ie_open('img', null, null, 'class', 'highlight-image', 'src', '/images/highlight_16.png', 'alt', 'Pack.apex', 'height', '200');
+    ie_close('img');
+    ie_close('div');
+    ie_close('section');
     ie_close('div');
     ie_close('div');
   }
